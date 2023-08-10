b9419bd2f628b32208666c82fa73ae7244bb5d39
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-08-10 18:48:04 +0300

    asserting-expected-values2

--- tut2/app/sample.py
+++ tut2/app/sample.py
@@ -1,3 +1,5 @@
 def validate_age(age):
     if age < 0 :
         raise ValueError ("Age Can not be less than 0")
+    
+# validate_age(-1)
--- tut2/test/test_sample.py
+++ tut2/test/test_sample.py
@@ -1,5 +1,21 @@
+import pytest
 from tut2.app.sample import validate_age
 
-def test_validate_age():
-    validate_age(10)
+# def test_validate_age():
+#     validate_age(10)
      
+# def test_validate_age_():
+#     with pytest.raises(ValueError) as exc_info:
+#         validate_age(-1)
+#     print(str(exc_info.value))
+
+
+# def test_validate_age_invalid_age():
+#     with pytest.raises(ValueError) as exc_info :
+#         validate_age(-1)
+#     assert str(exc_info.value) == "Age Can not be less than 0"  
+
+
+def test_validate_age_invalid_age():
+    with pytest.raises(ValueError, match="Age cannot be less than 0"):
+        validate_age(-1)
